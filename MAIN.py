ef8ace08adf06b15fbde47c9dcbbea3a1c7ec5e3
Anishkumar31 <anishkumar31804@gmail.com>
2023-03-05 15:52:29 +0530

    o

--- MAIN.py
+++ MAIN.py
@@ -4,12 +4,13 @@ from datetime import datetime, timedelta
 from tkinter import Tk, Label, PhotoImage, messagebox
 
 class WeatherApp:
-def _init_(self, api_key):
- self.api_key = api_key
-        self.city_name = None
- self.temperature = None
-        self.weather_description = None
-        self.forecast_data = None
+ def _init_(self, api_key):
+  self.api_key = api_key
+  self.city_name = None
+  self.temperature = None
+  self.weather_description = None
+  self.forecast_data = None
  def get_current_weather_data(self):
         weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city_name}&appid={self.api_key}"
+        
 
