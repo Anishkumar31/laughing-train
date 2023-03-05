e508b8c68106cb7146737358773cf8c0756dd946
Anishkumar31 <anishkumar31804@gmail.com>
2023-03-05 15:30:47 +0530

    anish

--- MAIN.py
+++ MAIN.py
@@ -2,7 +2,3 @@ import requests
 import json
 from datetime import datetime, timedelta
 from tkinter import Tk, Label, PhotoImage, messagebox
-print("saad")
-print("anish")
-print("prabhat")
-print("varun")
